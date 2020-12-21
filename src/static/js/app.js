7e6b34ddb80b7b1a24c8c8029bb5309ed2096b10
Phancisth <masterchief.purit@gmail.com>
2020-12-21 16:09:49 +0900

    Update app.js
    Heading added.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <H1>Todo App</H1>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
